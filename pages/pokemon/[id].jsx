9fccacbd5dc736e53a11957765e4b7e368afde14
kevinbre <bredeliskev@gmail.com>
2022-08-18 14:55:10 -0300

    Solved pokemon type images

--- pages/pokemon/[id].jsx
+++ pages/pokemon/[id].jsx
@@ -10,7 +10,7 @@ import {
   Container,
 } from "react-bootstrap";
 import Image from "next/image";
-// import pokemonType from "../mockup";
+import pokemonType from "../../src/mockup/pokeMockUp";
 
 function Pokemon() {
   const [myPokemon, setMyPokemon] = useState({});
@@ -35,14 +35,14 @@ function Pokemon() {
     });
   };
 
-  // const images = useCallback(() => {
-  //   const pokeType = myPokemon.type;
-  //   {
-  //     pokemonType
-  //       .filter((pokemon) => pokemon.type === pokeType)
-  //       .map((filteredPokemon) => setTypeImage(filteredPokemon.image));
-  //   }
-  // }, [myPokemon.type]);
+  const images = useCallback(() => {
+    const pokeType = myPokemon.type;
+    {
+      pokemonType
+        .filter((pokemon) => pokemon.type === pokeType)
+        .map((filteredPokemon) => setTypeImage(filteredPokemon.image));
+    }
+  }, [myPokemon.type]);
 
   useEffect(() => {
     pokemonInfo();
@@ -50,9 +50,9 @@ function Pokemon() {
     //eslint-disable-next-line react-hooks/exhaustive-deps
   }, [router.isReady]);
 
-  // useEffect(() => {
-  //   images();
-  // }, [images]);
+  useEffect(() => {
+    images();
+  }, [images]);
 
   return (
     <Container
@@ -86,12 +86,12 @@ function Pokemon() {
                 </Card.Title>
                 <Col className="d-flex justify-content-center">
                   {myPokemon.type}
-                  {/* <Image
+                  <Image
                     src={typeImage}
                     width={25}
                     height={25}
                     alt={myPokemon.type}
-                  /> */}
+                  />
                 </Col>
               </Row>
               <Row className="d-flex justify-content-center flex-column">
---
+++ src/mockup/pokeMockUp.jsx
@@ -0,0 +1,95 @@
+import Electric from "../images/Electric.png";
+import Tierra from "../images/Tierra.png";
+import Acero from "../images/Acero.png";
+import Agua from "../images/Agua.png";
+import Bicho from "../images/Tierra.png";
+import Dragon from "../images/Dragon.png";
+import Fantasma from "../images/Fantasma.png";
+import Hada from "../images/Hada.png";
+import Hielo from "../images/Hielo.png";
+import Lucha from "../images/Lucha.png";
+import Normal from "../images/Normal.png";
+import Planta from "../images/Planta.png";
+import Psiquico from "../images/Psiquico.png";
+import Roca from "../images/Roca.png";
+import Siniestro from "../images/Siniestro.png";
+import Veneno from "../images/Veneno.png";
+import Volador from "../images/Volador.png";
+import Fuego from "../images/Fuego.png";
+
+const pokemonType = [
+  {
+    image: Tierra,
+    type: "ground",
+  },
+  {
+    image: Electric,
+    type: "electric",
+  },
+  {
+    image: Acero,
+    type: "steel",
+  },
+  {
+    image: Agua,
+    type: "water",
+  },
+  {
+    image: Bicho,
+    type: "bug",
+  },
+  {
+    image: Dragon,
+    type: "dragon",
+  },
+  {
+    image: Fantasma,
+    type: "ghost",
+  },
+  {
+    image: Hada,
+    type: "fairy",
+  },
+  {
+    image: Hielo,
+    type: "ice",
+  },
+  {
+    image: Lucha,
+    type: "fighting",
+  },
+  {
+    image: Normal,
+    type: "normal",
+  },
+  {
+    image: Planta,
+    type: "grass",
+  },
+  {
+    image: Psiquico,
+    type: "psychic",
+  },
+  {
+    image: Roca,
+    type: "rock",
+  },
+  {
+    image: Siniestro,
+    type: "dark",
+  },
+  {
+    image: Veneno,
+    type: "poison",
+  },
+  {
+    image: Volador,
+    type: "flying",
+  },
+  {
+    image: Fuego,
+    type: "fire",
+  },
+];
+
+export default pokemonType;
